a838c97dd96851552550a6a429fbd14c2e303f41
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-06-21 15:30:52 +0900

    [연습문제]직사각형 별 찍기

--- 1level/[해시]완주하지_못한_선수.js
+++ 1level/[해시]완주하지_못한_선수.js
@@ -1,17 +1,5 @@
 function solution(participant, completion) {
   let answer;
-  for (let man of participant) {
-    if (completion.includes(man)) {
-      completion.splice(completion.indexOf(man), 1);
-    } else {
-      answer = man;
-    }
-  }
-  return answer;
-}
-
-function solution(participant, completion) {
-  let answer;
   participant.sort();
   completion.sort();
   for (let i = 0; i < participant.length; i++) {
--- test.js
+++
@@ -1,4 +0,0 @@
-let test = [1, 2, 3, 4, 5];
-console.log(test.splice(1, 1));
-console.log(test);
-console.log(test.indexOf(1));
