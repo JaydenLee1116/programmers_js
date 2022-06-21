d866302c26635c56449051bdba1ac52913942efa
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-06-21 15:53:27 +0900

    map의 2번째 인자(index)를 활용한 풀이 주석

--- 1level/[연습문제]x만큼 간격 n개의 숫자.js
+++ 1level/[연습문제]x만큼 간격 n개의 숫자.js
@@ -5,3 +5,11 @@ function solution(x, n) {
   }
   return answer;
 }
+
+/*
+function solution(x, n) {
+    return Array(n).fill(x).map((v, i) => (i + 1) * v)
+}
+
+map에서 첫번째 인자는 배열의 value, 두번째 인자는 배열의 index임을 활용한 풀이
+*/
