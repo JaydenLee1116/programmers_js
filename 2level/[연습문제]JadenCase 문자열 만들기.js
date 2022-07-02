0831fad7ebc4a765828eb0a7df6a21f72be21ca1
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-07-03 02:07:05 +0900

    [연습문제]JadenCase 문자열 만들기

---
+++ 2level/[연습문제]JadenCase 문자열 만들기.js
@@ -0,0 +1,30 @@
+// function solution(s) {
+//     s = s.toLowerCase();
+//     let words = s.split(' ');
+//     let answer = [];
+
+//     for (let word of words) {
+//         if (word === '') {
+
+//         } else {
+//             word = word[0].toUpperCase() + word.slice(1);
+//             answer.push(word);
+//         }
+//     }
+//     return answer.join(' ');
+// }
+
+function solution(s) {
+  s = s.split(' ').map((el) => el.toLowerCase());
+
+  s = s.map((v) => {
+    v = v.split('');
+
+    if (v[0] === undefined) return;
+    v[0] = v[0].toUpperCase();
+
+    return v.join('');
+  });
+
+  return s.join(' ');
+}
