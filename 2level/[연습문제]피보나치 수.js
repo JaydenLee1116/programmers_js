dc0c1dcb08bcf240ee6b0c0c6cd41152aa32db39
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-07-03 12:02:40 +0900

    [연습문제]피보나치 수

---
+++ 2level/[연습문제]피보나치 수.js
@@ -0,0 +1,26 @@
+console.log('jayden    hi'.split(' '));
+// function solution(n) {
+//     const fibonacci = (n) => {
+//         if (n === 0) {
+//             return 0;
+//         } else if (n === 1) {
+//             return 1;
+//         } else {
+//             return fibonacci(n - 2) % 1234567 + fibonacci(n - 1) % 1234567;
+//         }
+//     }
+
+//     return fibonacci(n) % 1234567;
+// }
+
+function solution(n) {
+  let fibonacciArr = new Array(n + 1);
+  fibonacciArr[0] = 0;
+  fibonacciArr[1] = 1;
+
+  for (let i = 2; i < n + 1; i++) {
+    fibonacciArr[i] = (fibonacciArr[i - 2] + fibonacciArr[i - 1]) % 1234567;
+  }
+
+  return fibonacciArr[n];
+}
