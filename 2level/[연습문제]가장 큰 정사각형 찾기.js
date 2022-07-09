e416f7bcd4f5ffbd7e876acf3eacdb68d40132f6
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-07-10 00:32:09 +0900

    [연습문제]가장 큰 정사각형 찾기(다시)

--- 2level/[연습문제]가장 큰 정사각형 찾기.js
+++ 2level/[연습문제]가장 큰 정사각형 찾기.js
@@ -1,22 +1,22 @@
 function solution(board) {
-  let answer = 0;
   let row = board.length;
-  let col = board[0].length;
+  let column = board[0].length;
 
-  if (row < 2 || col < 2) return 1;
+  if (row <= 1 || column <= 1) return 1;
 
+  let answerLength = 0;
   for (let i = 1; i < row; i++) {
-    for (let j = 1; j < col; j++) {
-      if (board[i][j] !== 0) {
-        let min = Math.min(
-          board[i - 1][j - 1],
-          board[i - 1][j],
-          board[i][j - 1]
-        );
-        board[i][j] = min + 1;
+    for (let j = 1; j < column; j++) {
+      if (board[i][j] >= 1) {
+        let cross = board[i - 1][j - 1];
+        let up = board[i - 1][j];
+        let left = board[i][j - 1];
+        if (cross >= 1 && up >= 1 && left >= 1) {
+          board[i][j] = Math.min(cross, up, left) + 1;
+        }
+        answerLength = Math.max(answerLength, board[i][j]);
       }
-      if (answer < board[i][j]) answer = board[i][j];
     }
   }
-  return answer * answer;
+  return answerLength ** 2;
 }
