a8925198e4e1ebcc9466229dc8121cc8556b6a77
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-10-12 12:55:09 +0900

    refactor: Upgrade 올바른 괄호

--- 2level/[연습문제]올바른 괄호.js
+++ 2level/[연습문제]올바른 괄호.js
@@ -17,3 +17,54 @@ function solution(s) {
     return answer;
   }
 }
+
+// 광탈 방지 A-Z에서 내 풀이(스택 사용)
+function solution(s) {
+  var answer = true;
+  var sLength = s.length;
+  var sArray = s.split('');
+  var count = 0;
+
+  for (let i = 0; i < sLength; i++) {
+    if (sArray.pop() === ')') {
+      count++;
+    } else {
+      count--;
+      if (count < 0) return false;
+    }
+  }
+
+  if (count !== 0) return false;
+
+  return answer;
+}
+
+// 광탈 방지 A-Z에서 강사님 풀이(스택 사용)
+function solution(s) {
+  let stack = [];
+
+  for (const v of s) {
+    if (v === '(') {
+      stack.push(v);
+    } else {
+      if (!stack.length) return false;
+      stack.pop();
+    }
+  }
+  return !stack.length;
+}
+
+// 광탈 방지 A-Z에서 강사님 풀이 변형(스택 개념 사용)
+function solution(s) {
+  let count = 0;
+
+  for (const v of s) {
+    if (v === '(') {
+      count++;
+    } else {
+      if (!count) return false;
+      count--;
+    }
+  }
+  return !count;
+}
