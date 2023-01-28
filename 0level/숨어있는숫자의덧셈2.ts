0fd1c3e227258e101be91d7b7e44aae47b1c90ce
JaydenLee1116 <vv55adss@gmail.com>
2023-01-28 09:33:51 +0900

    feat: Solve level-0 숨어있는숫자의덧셈2

---
+++ 0level/숨어있는숫자의덧셈2.ts
@@ -0,0 +1,8 @@
+const solution = (my_string: string): number => {
+  return my_string
+    .replace(/[A-z]/g, ',')
+    .split(',')
+    .reduce((acc, cur) => {
+      return cur === '' ? acc : acc + parseInt(cur);
+    }, 0);
+};
