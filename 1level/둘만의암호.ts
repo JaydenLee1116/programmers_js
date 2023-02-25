b0366b8f7f930fbb76196da167e2cba946a49b66
JaydenLee1116 <vv55adss@gmail.com>
2023-02-25 12:44:23 +0900

    feat: Add 1level 둘만의암호

---
+++ 1level/둘만의암호.ts
@@ -0,0 +1,11 @@
+{
+  const solution = (s: string, skip: string, index: number) => {
+    let alphabets = new Array(26).fill(0).map((_, i) => String.fromCharCode(i + 97));
+    let alphabetsExceptSkip = alphabets.filter((v) => !skip.includes(v));
+    return [...s]
+      .map((word, i, arr) => {
+        return alphabetsExceptSkip[(alphabetsExceptSkip.indexOf(word) + index) % alphabetsExceptSkip.length];
+      })
+      .join('');
+  };
+}
