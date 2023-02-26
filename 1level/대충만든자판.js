1ef34f2bb70a0a2f55e21f80ea42cc8220c05460
JaydenLee1116 <vv55adss@gmail.com>
2023-02-26 21:16:52 +0900

    feat: Add 1level 대충만든자판

---
+++ 1level/대충만든자판.js
@@ -0,0 +1,12 @@
+{
+    const solution = (keymap, targets) => {
+        return targets.map((target) => {
+            let result = [...target]
+                .map((word) => {
+                return Math.min(...keymap.map((key) => (key.indexOf(word) >= 0 ? key.indexOf(word) + 1 : Infinity)));
+            })
+                .reduce((acc, cur) => acc + cur);
+            return result === Infinity ? -1 : result;
+        });
+    };
+}
