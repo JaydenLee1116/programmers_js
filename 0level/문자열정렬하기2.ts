03caf695fc029d38a5cf0c3012fda809f130fabc
JaydenLee1116 <vv55adss@gmail.com>
2023-02-26 17:18:54 +0900

    test git

--- 0level/문자열정렬하기2.ts
+++ 0level/문자열정렬하기2.ts
@@ -2,5 +2,5 @@
   const solution = (my_string: string) => {
     return [...my_string.toLocaleLowerCase()].sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0)).join('');
   };
-  // 다시 테스트
+  // 왜 계속 name, password 요구하지?
 }
