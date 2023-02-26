2e7e3859947817aa5fb6dede4e2c3c28caa83a7e
JaydenLee1116 <vv55adss@gmail.com>
2023-02-26 17:19:49 +0900

    test last plz

--- 0level/문자열정렬하기2.ts
+++ 0level/문자열정렬하기2.ts
@@ -2,5 +2,4 @@
   const solution = (my_string: string) => {
     return [...my_string.toLocaleLowerCase()].sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0)).join('');
   };
-  // 왜 계속 name, password 요구하지?
 }
